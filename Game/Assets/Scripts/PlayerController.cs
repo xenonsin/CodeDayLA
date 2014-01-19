28ea2f0f4bacab6cc5b8fa38faa0bd62a17b2c46
xenonsin <kenoiwasp@gmail.com>
2014-01-18 16:13:23 -0800

    Initial Commit

---
+++ Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private float gravity = 8.0f;
+    private float startTime;
+	private float endTime = 5.0f;
+    private float TimeStamp;
+
+    private bool Right;
+  
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //StartCoroutine(move());
+
+        if (Right)
+        {
+
+            transform.Translate(new Vector2(2.0f * Time.deltaTime, 0f));
+
+            if (Time.time > TimeStamp)
+            {
+                TimeStamp = Time.time + 3.0f;
+                Right = false;
+            }
+        }
+        else
+        {
+ 
+            transform.Translate(new Vector2(-2.0f * Time.deltaTime, 0f));
+
+            if (Time.time > TimeStamp)
+            {
+                TimeStamp = Time.time + 3.0f;
+                Right = true;
+            }
+        }
+  
+     
+	
+	}
+}
